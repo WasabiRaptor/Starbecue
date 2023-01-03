b8cec3cd94cc4f0494b3a2bcf945aa635d9416d5
WasabiRaptor <ketchupraptor@gmail.com>
2023-01-03 08:34:43 -0500

    make this script more flexible

--- objects/sbq/setDirectivesTag.lua
+++ objects/sbq/setDirectivesTag.lua
@@ -1,8 +1,18 @@
 local _init = init or function() end
 function init()
 	local directives = config.getParameter("directives")
-	if directives then
+	if type(directives) == "string" then
 		animator.setGlobalTag("directives", directives)
+	elseif type(directives) == "table" then
+		for tag, value in pairs(directives) do
+			if type(value) == "string" then
+				animator.setGlobalTag(tag, value)
+			elseif type(value) == "table" then
+				for tag2, val in pairs(value) do
+					animator.setPartTag(tag, tag2, val)
+				end
+			end
+		end
 	end
 	_init()
 end
