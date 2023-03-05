e47665bafc52f1c3129e2371366fac41c41db00f
WasabiRaptor <ketchupraptor@gmail.com>
2023-03-05 04:32:37 -0500

    move that from player primary to deployment

--- scripts/SBQ_player_deployment.lua
+++ scripts/SBQ_player_deployment.lua
@@ -1,9 +1,13 @@
 ---@diagnostic disable: undefined-global
-local oldinit = init
-local oldupdate = update
+local old = {
+	init = init,
+	update = update
+}
+sbq = {}
+require("/scripts/SBQ_RPC_handling.lua")
 
-function init(...)
-	oldinit(...)
+function init()
+	old.init()
 
 	message.setHandler("sbqLight", function (_,_, light)
 		player.setProperty("sbqLight", light)
@@ -11,12 +15,35 @@ function init(...)
 			localAnimator.clearLightSources()
 		end
 	end)
+	message.setHandler("sbqPredatorDespawned", function(_, _, eaten, species, occupants)
+		sbq.timer("sbqPredatorDespawned", 0.5, function ()
+			status.setStatusProperty("sbqPreyList", nil)
+			world.sendEntityMessage(player.id(), "sbqRefreshSettings", player.getProperty( "sbqSettings") or {} )
+			world.sendEntityMessage(player.id(), "sbqLight")
 
+			if not eaten then
+				local resolvePosition = world.resolvePolyCollision(mcontroller.collisionPoly(), mcontroller.position(), 5)
+				if resolvePosition ~= nil then
+					mcontroller.setPosition(resolvePosition)
+				end
+
+				for i, effect in ipairs(root.assetJson("/sbqGeneral.config").predStatusEffects) do
+					status.removeEphemeralEffect(effect)
+				end
+				if species == "sbqOccupantHolder" and occupants == 0 and not (status.statusProperty("speciesAnimOverrideData") or {}).permanent then
+					status.clearPersistentEffects("speciesAnimOverride")
+				end
+			end
+
+			player.setProperty( "sbqCurrentData", nil)
+			status.setStatusProperty("sbqCurrentData", nil)
+		end)
+	end)
 end
 
-function update(dt, ...)
-	if oldupdate ~= nil then oldupdate(dt, ...) end
-
+function update(dt)
+	sbq.checkTimers(dt)
+	old.update(dt)
 	local light = player.getProperty( "sbqLight" )
 	if light ~= nil then
 		localAnimator.clearLightSources()
--- scripts/SBQ_player_primary.lua
+++ scripts/SBQ_player_primary.lua
@@ -4,39 +4,8 @@ local old = {
 }
 sbq = {}
 require("/scripts/SBQ_everything_primary.lua")
-require("/scripts/SBQ_RPC_handling.lua")
 
 function init()
 	old.init()
 	sbq.everything_primary()
-
-	message.setHandler("sbqPredatorDespawned", function(_, _, eaten, species, occupants)
-		sbq.timer("sbqPredatorDespawned", 0.5, function ()
-			status.setStatusProperty("sbqPreyList", nil)
-			world.sendEntityMessage(player.id(), "sbqRefreshSettings", player.getProperty( "sbqSettings") or {} )
-			world.sendEntityMessage(player.id(), "sbqLight")
-
-			if not eaten then
-				local resolvePosition = world.resolvePolyCollision(mcontroller.collisionPoly(), mcontroller.position(), 5)
-				if resolvePosition ~= nil then
-					mcontroller.setPosition(resolvePosition)
-				end
-
-				for i, effect in ipairs(root.assetJson("/sbqGeneral.config").predStatusEffects) do
-					status.removeEphemeralEffect(effect)
-				end
-				if species == "sbqOccupantHolder" and occupants == 0 and not (status.statusProperty("speciesAnimOverrideData") or {}).permanent then
-					status.clearPersistentEffects("speciesAnimOverride")
-				end
-			end
-
-			player.setProperty( "sbqCurrentData", nil)
-			status.setStatusProperty( "sbqCurrentData", nil)
-		end)
-	end)
-end
-
-function update(dt)
-	sbq.checkTimers(dt)
-	old.update(dt)
 end
