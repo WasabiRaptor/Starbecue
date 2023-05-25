df8a5de8dee2c65cdfe8056c80d133aa52fcad4b
WasabiRaptor <ketchupraptor@gmail.com>
2023-05-25 17:16:57 -0400

    fix the settings check function here

--- vehicles/sbq/sbq_general_functions.lua
+++ vehicles/sbq/sbq_general_functions.lua
@@ -65,8 +65,37 @@ function sbq.randomChance(percent)
 	return math.random() <= (percent/100)
 end
 
-require("/scripts/SBQ_check_settings.lua")
+function sbq.checkSettings(checkSettings)
+	local settings = sbq.settings
+	for setting, value in pairs(checkSettings or {}) do
+		if (type(settings[setting]) == "table") and settings[setting].name ~= nil then
+			if not value then return false
+			elseif type(value) == "table" then
+				if not sbq.checkTable(value, settings[setting]) then return false end
+			end
+		elseif type(value) == "table" then
+			local match = false
+			for i, value in ipairs(value) do if (settings[setting] or false) == value then
+				match = true
+				break
+			end end
+			if not match then return false end
+		elseif (settings[setting] or false) ~= value then return false
+		end
+	end
+	return true
+end
 
+function sbq.checkTable(check, checked)
+	for k, v in pairs(check) do
+		if type(v) == "table" then
+			if not sbq.checkTable(v, (checked or {})[k]) then return false end
+		elseif v == true and type((checked or {})[k]) ~= "boolean" and ((checked or {})[k]) ~= nil then
+		elseif not (v == (checked or {})[k] or false) then return false
+		end
+	end
+	return true
+end
 
 function sbq.localToGlobal( position )
 	local lpos = { position[1], position[2] }
