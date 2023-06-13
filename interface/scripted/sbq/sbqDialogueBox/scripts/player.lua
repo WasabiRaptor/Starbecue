8bf6029871fc093a6c400979d523b703c601e7f0
WasabiRaptor <ketchupraptor@gmail.com>
2023-06-13 18:56:48 -0400

    vore crew should work again

--- interface/scripted/sbq/sbqDialogueBox/scripts/player.lua
+++ interface/scripted/sbq/sbqDialogueBox/scripts/player.lua
@@ -80,17 +80,17 @@ function dialogueBoxScripts.swapFollowing(dialogueTree, dialogueTreeTop, setting
 	sbq.addRPC(world.sendEntityMessage(pane.sourceEntity(), "sbqSwapFollowing"), function(data)
 		if data and data[1] then
 			if data[1] == "None" then
-				sbq.updateDialogueBox({}, dialogueTree.continue)
+				sbq.updateDialogueBox(".continue", dialogueTree, dialogueTreeTop)
 			elseif data[1] == "Message" then
 				if data[2].messageType == "recruits.requestUnfollow" then
 					world.sendEntityMessage(player.id(), "recruits.requestUnfollow", table.unpack(data[2].messageArgs))
-					sbq.updateDialogueBox({}, dialogueTree.continue)
+					sbq.updateDialogueBox(nil, dialogueTree.continue)
 				elseif data[2].messageType == "recruits.requestFollow" then
 					local result = world.sendEntityMessage(player.id(), "sbqRequestFollow", table.unpack(data[2].messageArgs)):result()
 					if result == nil then
-						sbq.updateDialogueBox({}, dialogueTree.continue)
+						sbq.updateDialogueBox(".continue", dialogueTree, dialogueTreeTop)
 					else
-						sbq.updateDialogueBox({}, dialogueTree.fail)
+						sbq.updateDialogueBox(".fail", dialogueTree, dialogueTreeTop)
 					end
 				end
 			end
