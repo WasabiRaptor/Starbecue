013de0c9afebf9cc95057b4daa208ed6c99709ce
WasabiRaptor <ketchupraptor@gmail.com>
2023-07-07 17:06:03 -0400

    Auri Shop dialogue should be fixed

--- stagehands/coordinator/SBQ_VoreNPC_Combat.lua
+++ stagehands/coordinator/SBQ_VoreNPC_Combat.lua
@@ -1,7 +1,7 @@
 local _npcCombat = npcCombat
 
 function npcCombat(dt)
-	if world.entityExists(self.goal) and world.entityType(self.goal) ~= "player" and ((world.callScriptedEntity(self.goal, "status.statusProperty", ("sbqCurrentData")) or {}).type == "prey") then
+	if world.entityExists(self.goal) and world.entityType(self.goal) ~= "player" and ((world.callScriptedEntity(self.goal, "status.statusProperty", ("sbqType"))) == "prey") then
 		self.success = true
 		return
 	end
