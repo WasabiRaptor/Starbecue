c75f88ea6334a6051b05c292981d610cbdb3e6df
WasabiRaptor <ketchupraptor@gmail.com>
2024-01-06 17:45:44 -0500

    addDirectives moved over, needs to be repurposed

--- scripts/SBQ_humanoidAnimator.lua
+++ scripts/SBQ_humanoidAnimator.lua
@@ -1,6 +1,6 @@
 local old = {
-    initAnimator = initAnimator,
-	equipmentSlotUpdated = equipmentSlotUpdated
+    initAnimator = initAnimator or (function ()end),
+	equipmentSlotUpdated = equipmentSlotUpdated or (function ()end)
 }
 require("/scripts/SBQ_check_settings.lua")
 
@@ -50,6 +50,37 @@ function sbq.remapColor(remaps, fromMap, toMap)
 			directives = directives .. "?replace"
 		end
     end
-	sb.logInfo(((directives ~= "?replace") and directives) or "")
 	return ((directives ~= "?replace") and directives) or ""
+end
+
+-- this function in it's current state was made explicitly to add a missing color to familar's palettes
+-- however, it should probably be repurposed to add a missing color to all palettes
+function sbq.addDirectives()
+	local directives = self.identity.bodyDirectives:lower()
+	local found1, found2 = directives:find("00ffa1=")
+	if found1 then
+		local colorStartIndex = found2+1
+		local colorEndIndex = #directives
+		local found4 = directives:find(";", colorStartIndex)
+		if found4 then
+			local found5 = directives:find("?", colorStartIndex)
+			if found5 and found4 > found5 then
+				colorEndIndex = found5 -1
+			else
+				colorEndIndex = found4 -1
+			end
+		end
+
+		local multiplyAmount = 0.75
+		local color = directives:sub(colorStartIndex, colorEndIndex)
+		local R = tonumber(color:sub(1,2),16)
+		local G = tonumber(color:sub(3,4),16)
+		local B = tonumber(color:sub(5,6),16)
+		local A = ""
+		if #color == 8 then
+			A = color:sub(7,8)
+		end
+		local newReplaceColors = "?replace;00c77d="..string.format("%02x", math.floor(R * multiplyAmount))..string.format("%02x", math.floor(G * multiplyAmount))..string.format("%02x", math.floor(B * multiplyAmount))..A
+		self.identity.bodyDirectives = self.identity.bodyDirectives..newReplaceColors
+	end
 end
