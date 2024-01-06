7f1676a695c40860d20ef9a83ffd0946412e9b1e
WasabiRaptor <ketchupraptor@gmail.com>
2024-01-06 00:31:47 -0500

    remove anim override files

--- humanoid/dragon/detect_reskin2.lua
+++
@@ -1,7 +0,0 @@
-
-if type(self.speciesFile) == "table" then
-	if self.speciesFile.charGenTextLabels[1] == "Eye colour" and self.speciesFile.charGenTextLabels[6] == "Detail colour" then
-
-		self.speciesData = sb.jsonMerge(self.speciesData, root.assetJson("/humanoid/dragon/draconisdragons/sbqAnimOverrideParts.config") )
-	end
-end
