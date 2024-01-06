c4651fc15cd750ceb7643916e8c9e87c9c6b2a61
WasabiRaptor <ketchupraptor@gmail.com>
2024-01-06 03:19:14 -0500

    potions won't log an error about no unique sprites anymore

--- items/active/sbqTransformation/sbqMysteriousPotion/sbqMysteriousPotionBuilder.lua
+++ items/active/sbqTransformation/sbqMysteriousPotion/sbqMysteriousPotionBuilder.lua
@@ -1,39 +1,25 @@
-
+local speciesFile
 function build(directory, config, parameters, level, seed)
 
 	if parameters ~= nil then
 		if parameters.species then
-			local success, speciesFile = pcall(root.assetJson, ("/species/"..parameters.species..".species") )
-			if success then
-				config.shortdescription = speciesFile.charCreationTooltip.title.." Potion"
+			speciesFile = root.speciesConfig(parameters.species)
+			if speciesFile then
+                config.shortdescription = speciesFile.charCreationTooltip.title .. " Potion"
+				parameters.potionPath = speciesFile.potionPath or parameters.potionPath
 			end
 		end
 		if parameters.name then
 			config.shortdescription = parameters.name.." Potion"
 		end
-		local directives = ((parameters.directives or "")..((parameters.identity or {}).bodyDirectives or ""))..(parameters.potionDirectives or "")
-		local largeDirectives = ((parameters.directives or "")..((parameters.identity or {}).bodyDirectives or ""))..(parameters.potionDirectives or "")
-
-		if parameters.species and not parameters.specialPotionIcon then
-			local success, notEmpty = pcall(root.nonEmptyRegion, "/humanoid/"..(parameters.species).."/sbqPotion.png")
-			if success and notEmpty ~= nil then
-				parameters.potionPath = "/humanoid/"..(parameters.species).."/"
-			end
-		end
+		local directives = ((parameters.directives or "")..(parameters.bodyDirectives or "")..(parameters.secondaryDirectives or ""))
 		if parameters.potionPath then
-			local success, notEmpty = pcall(root.nonEmptyRegion, parameters.potionPath.."/sbqPotionBlend.png")
-			if success and notEmpty ~= nil then
-				directives = directives.."?blendmult="..parameters.potionPath.."/sbqPotionBlend.png"
-				largeDirectives = largeDirectives.."?blendmult="..parameters.potionPath.."/sbqPotionLargeBlend.png"
-			end
-
 			config.inventoryIcon = parameters.potionPath.."sbqPotion.png"..directives
-			config.largeImage = parameters.potionPath.."sbqPotionLarge.png"..largeDirectives
+			config.largeImage = parameters.potionPath.."sbqPotionLarge.png"..directives
 			config.animationParts = {
 				potion = parameters.potionPath.."sbqPotion.png"..directives
 			}
 		end
 	end
-
 	return config, parameters
 end
--- items/active/sbqTransformation/sbqSpeciesPotion/sbqSpeciesPotion.lua
+++ items/active/sbqTransformation/sbqSpeciesPotion/sbqSpeciesPotion.lua
@@ -9,7 +9,6 @@ function update(dt, fireMode, shiftHeld)
 		local speciesAnimOverrideData = status.statusProperty("speciesAnimOverrideData") or {}
 		data.species = speciesAnimOverrideData.species or world.entitySpecies(entity.id())
 		data.potionPath = "/items/active/sbqTransformation/sbqSpeciesPotion/"
-		data.potionDirectives = (speciesAnimOverrideData.directives or "")..(((speciesAnimOverrideData.identity or {})).bodyDirectives or "")
 		data.rarity = "rare"
 		player.giveItem({name = "sbqMysteriousPotion", parameters = data})
 		item.consume(1)
