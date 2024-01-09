4d68b4aedb812ef243b691fd7b2cd58df6ce6962
WasabiRaptor <ketchupraptor@gmail.com>
2024-01-09 14:33:03 -0500

    get ready for settings here

--- scripts/SBQ_humanoidAnimator.lua
+++ scripts/SBQ_humanoidAnimator.lua
@@ -5,17 +5,17 @@ local old = {
 require("/scripts/SBQ_check_settings.lua")
 
 function equipmentSlotUpdated(slot, itemDescriptor)
-	old.equipmentSlotUpdated(slot, itemDescriptor)
+    old.equipmentSlotUpdated(slot, itemDescriptor)
+    slotFunc = sbq[slot.."Slot"] or function() end
+	slotFunc(itemDescriptor)
 end
 
-
 function initAnimator()
+	old.initAnimator()
+
 	local defaultColorMap = root.speciesConfig("human").baseColorMap
     local speciesConfig = root.speciesConfig(humanoid.species())
-
 	sbq.animOverrideSettings = sb.jsonMerge(speciesConfig.animOverrideDefaultSettings or {}, status.statusProperty("speciesAnimOverrideSettings") or {}, status.statusProperty("speciesAnimOverrideOverrideSettings") or {})
-
-    old.initAnimator()
 	for tag, remaps in pairs(speciesConfig.colorRemapGlobalTags or {}) do
 		animator.setGlobalTag(tag, sbq.remapColor(remaps, defaultColorMap, speciesConfig.baseColorMap or defaultColorMap))
     end
@@ -23,8 +23,64 @@ function initAnimator()
 		for tag, remaps in pairs(tags or {}) do
 			animator.setPartTag(part, tag, sbq.remapColor(remaps, defaultColorMap, speciesConfig.baseColorMap or defaultColorMap))
 		end
+    end
+    sbq.chestCosmeticSlot(humanoid.getItemSlot("chestCosmetic"))
+	sbq.legsCosmeticSlot(humanoid.getItemSlot("legsCosmetic"))
+	sbq.headCosmeticSlot(humanoid.getItemSlot("headCosmetic"))
+	sbq.backCosmeticSlot(humanoid.getItemSlot("backCosmetic"))
+end
+
+function sbq.chestSlot(itemDescriptor)
+	if not humanoid.getItemSlot("chestCosmetic") then
+		sbq.chestCosmeticSlot(itemDescriptor)
 	end
 end
+function sbq.legsSlot(itemDescriptor)
+	if not humanoid.getItemSlot("legsCosmetic") then
+		sbq.legsCosmeticSlot(itemDescriptor)
+	end
+end
+function sbq.headSlot(itemDescriptor)
+	if not humanoid.getItemSlot("headCosmetic") then
+		sbq.headCosmeticSlot(itemDescriptor)
+	end
+end
+function sbq.backSlot(itemDescriptor)
+	if not humanoid.getItemSlot("backCosmetic") then
+		sbq.backCosmeticSlot(itemDescriptor)
+	end
+end
+
+function sbq.chestCosmeticSlot(itemDescriptor)
+	if not itemDescriptor then itemDescriptor = humanoid.getItemSlot("chest") end
+    if itemDescriptor then
+		local item = root.itemConfig(itemDescriptor)
+	else
+	end
+end
+function sbq.legsCosmeticSlot(itemDescriptor)
+	if not itemDescriptor then itemDescriptor = humanoid.getItemSlot("legs") end
+    if itemDescriptor then
+		local item = root.itemConfig(itemDescriptor)
+	else
+	end
+end
+function sbq.headCosmeticSlot(itemDescriptor)
+	if not itemDescriptor then itemDescriptor = humanoid.getItemSlot("head") end
+    if itemDescriptor then
+		local item = root.itemConfig(itemDescriptor)
+	else
+	end
+end
+function sbq.backCosmeticSlot(itemDescriptor)
+	if not itemDescriptor then itemDescriptor = humanoid.getItemSlot("back") end
+    if itemDescriptor then
+		local item = root.itemConfig(itemDescriptor)
+	else
+	end
+end
+
+
 
 function sbq.remapColor(remaps, fromMap, toMap)
 	local directives = "?replace"
