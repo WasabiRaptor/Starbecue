9e46857b104025d20671647e0aca91dd7207c667
WasabiRaptor <ketchupraptor@gmail.com>
2024-03-11 21:05:13 -0400

    icons and descriptions for the upgrade candy

--- items/active/sbqCandy/build.lua
+++ items/active/sbqCandy/build.lua
@@ -1,11 +1,13 @@
 function build(directory, config, parameters, level, seed)
 	local strings = root.assetJson("/sbqStrings.config")
-	parameters.level = math.max(parameters.level or 0, level or 0, 1)
+    parameters.level = math.max(parameters.level or 0, level or 0, 1)
+	parameters.seed = parameters.seed or seed
     config = sb.jsonMerge(config, parameters)
+    math.randomseed(parameters.seed);
 
     config.inventoryIcon = "sbqCandyT" .. parameters.level .. ".png"
 	if not root.assetExists(directory..config.inventoryIcon) then
-		config.inventoryIcon = "sbqMysteryCandy.png"
+		config.inventoryIcon = "sbqCandyT"..math.random(1,6)".png?hueshift="..math.random(0,360)..";"
 	end
     config.shortdescription = strings.candyName[config.level] or strings.mysteryCandyName
     config.description = strings.candyDesc[config.level] or strings.mysteryCandyDesc
