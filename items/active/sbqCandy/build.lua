d00d4949e0d74d445a4840722590ef4839ccc73f
WasabiRaptor <ketchupraptor@gmail.com>
2024-03-12 08:45:53 -0400

    small fix here too

--- items/active/sbqCandy/build.lua
+++ items/active/sbqCandy/build.lua
@@ -1,13 +1,13 @@
 function build(directory, config, parameters, level, seed)
 	local strings = root.assetJson("/sbqStrings.config")
     parameters.level = math.max(parameters.level or 0, level or 0, 1)
-	parameters.seed = parameters.seed or seed
+	parameters.seed = parameters.seed or seed or parameters.level
     config = sb.jsonMerge(config, parameters)
     math.randomseed(parameters.seed);
 
     config.inventoryIcon = "sbqCandyT" .. parameters.level .. ".png"
 	if not root.assetExists(directory..config.inventoryIcon) then
-		config.inventoryIcon = "sbqCandyT"..math.random(1,6)".png?hueshift="..math.random(0,360)..";"
+		config.inventoryIcon = "sbqCandyT"..math.random(1,6)..".png?hueshift="..math.random(0,360)..";"
 	end
     config.shortdescription = strings.candyName[config.level] or strings.mysteryCandyName
     config.description = strings.candyDesc[config.level] or strings.mysteryCandyDesc
