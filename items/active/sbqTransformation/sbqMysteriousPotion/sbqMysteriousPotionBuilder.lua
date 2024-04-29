d7cf0785996935aa8e62c2f479d7472d0afb10c5
WasabiRaptor <ketchupraptor@gmail.com>
2024-04-29 09:49:50 -0400

    better potion handling

--- items/active/sbqTransformation/sbqMysteriousPotion/sbqMysteriousPotionBuilder.lua
+++ items/active/sbqTransformation/sbqMysteriousPotion/sbqMysteriousPotionBuilder.lua
@@ -2,15 +2,23 @@ local speciesFile
 local strings
 function build(directory, config, parameters, level, seed)
 	local config = sb.jsonMerge(config, parameters or {})
-	strings = root.assetJson("/sbqStrings.config")
-	if config.args and config.args[1] then
-		local identity = config.args[1]
-		if identity.species then
-			if identity.species == "any" then
+    strings = root.assetJson("/sbqStrings.config")
+
+    config.identity = parameters.identity or (parameters.args or {})[1]
+    config.duration = parameters.duration or (parameters.args or {})[2]
+
+    parameters.args = {
+        config.identity,
+		config.duration
+	}
+
+	if config.identity then
+		if config.identity.species then
+			if config.identity.species == "any" then
 				config.shortdescription = strings.mysteryPotionName
 				config.description = strings.mysteryPotionDesc
 			else
-				speciesFile = root.speciesConfig(identity.species)
+				speciesFile = root.speciesConfig(config.identity.species)
 				if speciesFile then
 					config.shortdescription = sb.replaceTags(strings.speciesPotionName, {species = speciesFile.charCreationTooltip.title, duration = tostring(config.duration)})
 					config.description = sb.replaceTags(strings.speciesPotionDesc, {species = speciesFile.charCreationTooltip.title, duration = tostring(config.duration)})
@@ -22,9 +30,9 @@ function build(directory, config, parameters, level, seed)
 				end
 			end
 		end
-		if identity.name then
-			config.shortdescription = sb.replaceTags(strings.duplicatePotionName, { name = identity.name, duration = tostring(config.duration) })
-			config.description = sb.replaceTags(strings.duplicatePotionDesc, {name = identity.name, duration = tostring(config.duration)})
+		if config.identity.name then
+			config.shortdescription = sb.replaceTags(strings.duplicatePotionName, { name = config.identity.name, duration = tostring(config.duration) })
+			config.description = sb.replaceTags(strings.duplicatePotionDesc, {name = config.identity.name, duration = tostring(config.duration)})
 		end
 	end
 	parameters.animationParts = {
