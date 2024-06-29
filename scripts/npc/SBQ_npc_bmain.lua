2f2b56f63b34ce57f067fad9258f01b962ed607b
WasabiRaptor <ketchupraptor@gmail.com>
2024-06-29 15:17:47 -0400

    NPCs in dome should spawn as micros, NPC should preserve scale when converting type

--- scripts/npc/SBQ_npc_bmain.lua
+++ scripts/npc/SBQ_npc_bmain.lua
@@ -140,7 +140,8 @@ function sbq.tenant_setNpcType(npcType)
 				initialStorage = preservedStorage(),
 				uniqueId = uuid,
 				sbqOverrideUniqueId = uuid
-			}
+			},
+			scale = mcontroller.scale()
 		},
 		storage = storage
 	}
