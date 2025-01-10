b69ea3cc79f6f90269327e07a7e9109b792ac4a3
WasabiRaptor <ketchupraptor@gmail.com>
2025-01-10 18:52:57 -0500

    more minor eye tracking updates

--- humanoid/any/eyeTracking.lua
+++ humanoid/any/eyeTracking.lua
@@ -1,19 +1,21 @@
 function sbq.eyeTracking()
 	local targetDistance = sbq.globalToLocal(sbq.targetPosition(), animator.partPoint("eyes", "eyePos"))
 	local targetAngle = vec2.angle(targetDistance) * 180 / math.pi
-	local absDist = {math.abs(targetDistance[1]), math.abs(targetDistance[2])}
-	if (absDist[1] > (sbq.voreConfig.eyeTrackingDeadzone[1])) then
+	local absDist = { math.abs(targetDistance[1]), math.abs(targetDistance[2]) }
+	local deadZone = animator.partProperty("eyes", "eyeDeadZone") or sbq.voreConfig.eyeTrackingDeadzone or {1,1}
+	local farDist = animator.partProperty("eyes", "eyeFarDist") or sbq.voreConfig.eyeTrackingFarDist or 10
+	if (absDist[1] > (deadZone[1])) then
 		if ((targetAngle > 292) or (targetAngle < 68)) then
-			animator.setGlobalTag("eyesX", (absDist[1] > (sbq.voreConfig.eyeTrackingFarDist or 10)) and "2" or "1")
+			animator.setGlobalTag("eyesX", (absDist[1] > farDist) and "2" or "1")
 		elseif ((targetAngle > 112) and (targetAngle < 248)) then
-			animator.setGlobalTag("eyesX", (absDist[1] > (sbq.voreConfig.eyeTrackingFarDist or 10)) and "-2" or "-1")
+			animator.setGlobalTag("eyesX", (absDist[1] > farDist) and "-2" or "-1")
 		else
 			animator.setGlobalTag("eyesX", "0")
 		end
 	else
 		animator.setGlobalTag("eyesX", "0")
 	end
-	if (absDist[2] > (sbq.voreConfig.eyeTrackingDeadzone[2])) then
+	if (absDist[2] > (deadZone[2])) then
 		if ((targetAngle > 22) and (targetAngle < 158)) then
 			animator.setGlobalTag("eyesY", "1")
 		elseif ((targetAngle > 202) and (targetAngle < 338)) then
