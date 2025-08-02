a628bca6c422e5f547a037e31bb8d53fc5c8dc74
WasabiRaptor <ketchupraptor@gmail.com>
2025-08-02 15:40:45 -0400

    remove metroid doors
    (I am putting them in their own mod)

--- objects/wasabiRaptor/metroidDoor/metroidDoorCollission.lua
+++
@@ -1,8 +0,0 @@
-function die()
-	local offset = config.getParameter("doorOffset") or {0,0}
-	local position = object.position()
-	local entity = world.objectAt({position[1]-offset[1],position[2]-offset[2]})
-	if type(entity) == "number" then
-		world.callScriptedEntity(entity, "openDoor")
-	end
-end
