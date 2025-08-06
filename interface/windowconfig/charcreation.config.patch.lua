f754bd104768e2b4728f9cfb85aaee4e4e35aa29
WasabiRaptor <ketchupraptor@gmail.com>
2025-08-06 15:28:25 -0400

    remove nickit, flareon, vaporeon, meowscarada

--- interface/windowconfig/charcreation.config.patch.lua
+++ interface/windowconfig/charcreation.config.patch.lua
@@ -2,10 +2,6 @@
 function patch(config)
     local speciesOrdering = {
 		"sbq/FFWizard/hellhound",
-		"sbq/vaporeon",
-		"sbq/flareon",
-		"sbq/meowscarada",
-		"sbq/nickit_anthro"
 		-- "sbq/slime",
     }
     for _, v in ipairs(speciesOrdering) do
