dbea32339f8e9bbf559a35409134c50cec3488ac
WasabiRaptor <ketchupraptor@gmail.com>
2025-08-25 03:47:08 -0400

    quick settings should always show pred options

--- interface/scripted/sbq/settings/buildQuickSettings.lua
+++ interface/scripted/sbq/settings/buildQuickSettings.lua
@@ -4,5 +4,15 @@ local sbq = world.sendEntityMessage(player.id(), "sbqSettingsPageData"):result()
 cfg.inputData = {
 	sbq = sbq
 }
+local sbqConfig = root.assetJson("/sbq.config")
+sbq.voreConfig = sbq.voreConfig or {}
+sbq.voreConfig.availableVoreTypes = sbq.voreConfig.availableVoreTypes or {}
+sbq.voreConfig.availableInfuseTypes = sbq.voreConfig.availableInfuseTypes or {}
+for k, v in pairs(sbqConfig.voreTypeData) do
+	sbq.voreConfig.availableVoreTypes[k] = true
+end
+for k, v in pairs(sbqConfig.infuseTypeData) do
+	sbq.voreConfig.availableInfuseTypes[k] = true
+end
 
 table.insert(cfg.children, #cfg.children, root.assetJson("/interface/scripted/sbq/settings/tabs/preferences.config:contents")[1])
