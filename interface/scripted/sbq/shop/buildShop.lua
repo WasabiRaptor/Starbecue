ec2397d0837174e42f614d57adc7969732456770
WasabiRaptor <ketchupraptor@gmail.com>
2025-09-03 08:59:26 -0400

    fix shop ui

--- interface/scripted/sbq/shop/buildShop.lua
+++ interface/scripted/sbq/shop/buildShop.lua
@@ -1,8 +1,16 @@
 cfg = root.assetJson("/interface/scripted/sbq/shop/shop.ui")
 
 local dialogueBoxUi = root.assetJson("/interface/scripted/sbq/dialogueBox/dialogueBox.ui")
+local function assetPath(path, directory)
+	if string.sub(path, 1, 1) == "/" then
+		return path
+	else
+		return (directory or "/") .. path
+	end
+end
+
 for _, v in ipairs(dialogueBoxUi.scripts) do
-	table.insert(cfg.scripts, sbq.assetPath(v, "/interface/scripted/sbq/dialogueBox/"))
+	table.insert(cfg.scripts, assetPath(v, "/interface/scripted/sbq/dialogueBox/"))
 end
 
 table.insert(cfg.children, {
