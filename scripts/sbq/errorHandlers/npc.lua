9bed24a3faed639b7443c86c4f7d26fd54a7d538
WasabiRaptor <ketchupraptor@gmail.com>
2025-09-10 14:57:56 -0400

    case insensitive

--- scripts/sbq/errorHandlers/npc.lua
+++ scripts/sbq/errorHandlers/npc.lua
@@ -1,21 +1,21 @@
 local oldIdMap = {
-    ["sbq/flareon"] = "wr/flareon",
-    ["sbq/vaporeon"] = "wr/vaporeon",
-    ["sbq/meowscarada"] = "wr/meowscarada",
-    ["sbq/nickit_anthro"] = "wr/nickit",
+	["sbq/flareon"] = "wr/flareon",
+	["sbq/vaporeon"] = "wr/vaporeon",
+	["sbq/meowscarada"] = "wr/meowscarada",
+	["sbq/nickit_anthro"] = "wr/nickit",
 }
 function error(diskStore, e)
-    if oldIdMap[diskStore.npcVariant.species] then
-        diskStore.npcVariant.species = oldIdMap[diskStore.npcVariant.species]
-    end
-    if oldIdMap[diskStore.npcVariant.humanoidIdentity.species] then
-        diskStore.npcVariant.humanoidIdentity.species = oldIdMap[diskStore.npcVariant.humanoidIdentity.species]
-    end
-    local overrideSpecies = sb.jsonQuery(diskStore.npcVariant, "overrides.identity.species")
-    if overrideSpecies then
-        if oldIdMap[overrideSpecies] then
-            diskStore.npcVariant.overrides.identity.species = oldIdMap[overrideSpecies]
-        end
-    end
-    return diskStore
+	if oldIdMap[diskStore.npcVariant.species:lower()] then
+		diskStore.npcVariant.species = oldIdMap[diskStore.npcVariant.species:lower()]
+	end
+	if oldIdMap[diskStore.npcVariant.humanoidIdentity.species:lower()] then
+		diskStore.npcVariant.humanoidIdentity.species = oldIdMap[diskStore.npcVariant.humanoidIdentity.species:lower()]
+	end
+	local overrideSpecies = sb.jsonQuery(diskStore.npcVariant, "overrides.identity.species")
+	if overrideSpecies then
+		if oldIdMap[overrideSpecies:lower()] then
+			diskStore.npcVariant.overrides.identity.species = oldIdMap[overrideSpecies:lower()]
+		end
+	end
+	return diskStore
 end
--- scripts/sbq/errorHandlers/player.lua
+++ scripts/sbq/errorHandlers/player.lua
@@ -2,12 +2,11 @@ local oldIdMap = {
     ["sbq/flareon"] = "wr/flareon",
     ["sbq/vaporeon"] = "wr/vaporeon",
     ["sbq/meowscarada"] = "wr/meowscarada",
-    ["sbq/nickit_anthro"] = "wr/nickit",
-    ["sbq/zygahedron/zevi"] = "sbq/Zygahedron/Zevi",
+    ["sbq/nickit_anthro"] = "wr/nickit"
 }
 function error(diskStore, e)
-    if oldIdMap[diskStore.identity.species] then
-        diskStore.identity.species = oldIdMap[diskStore.identity.species]
+    if oldIdMap[diskStore.identity.species:lower()] then
+        diskStore.identity.species = oldIdMap[diskStore.identity.species:lower()]
     end
     return diskStore
 end
