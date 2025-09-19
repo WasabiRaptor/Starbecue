f97f0b5b9d1099ac367b20a59371ad58e4ed668c
WasabiRaptor <ketchupraptor@gmail.com>
2025-09-19 00:21:46 -0400

    make that behave nicely

--- terrestrial_worlds.config.patch.lua
+++ terrestrial_worlds.config.patch.lua
@@ -1,7 +1,11 @@
 
 function patch(config)
-	table.insert(config.planetTypes.garden.layers.surface.dungeons, {1.0, "sbqGardenFireplace"})
-	config.planetTypes.garden.layers.surface.dungeonCountRange[1] = #config.planetTypes.garden.layers.surface.dungeons
-	config.planetTypes.garden.layers.surface.dungeonCountRange[2] = #config.planetTypes.garden.layers.surface.dungeons
+	if sb.jsonQuery(config, "planetTypes.garden.layers.surface.dungeons") then
+		table.insert(config.planetTypes.garden.layers.surface.dungeons, { 1.0, "sbqGardenFireplace" })
+	end
+	if sb.jsonQuery(config, "config.planetTypes.garden.layers.surface.dungeonCountRange") then
+		config.planetTypes.garden.layers.surface.dungeonCountRange[1] = config.planetTypes.garden.layers.surface.dungeonCountRange[1] + 1
+		config.planetTypes.garden.layers.surface.dungeonCountRange[2] = config.planetTypes.garden.layers.surface.dungeonCountRange[2] + 1
+	end
 	return config
 end
