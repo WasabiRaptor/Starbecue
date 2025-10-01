3d92c2b1422788f2241937175a1959c6d460519d
WasabiRaptor <ketchupraptor@gmail.com>
2025-10-01 10:53:40 -0400

    only use fmod when the index is greater than the table length

--- species/sbq/LakotaAmitola/ziellekDragon.lua
+++ species/sbq/LakotaAmitola/ziellekDragon.lua
@@ -1,6 +1,10 @@
 -- these are just the names of the variables internally and how they're used on the creation screen, do whatever you want with them here!
 -- but probably put name species and gender as the ones supplied, thats probably a good idea
 -- I've supplied the bare minimum as a template here, do with it what you will
+local function wrap(index, array)
+	if index < #array then return index end
+	return math.fmod(index, #array)
+end
 function create(name, species, genderIndex, primaryColor, accentColor, hornColor, fluffColor, bellyColor, beansColor, eyeColor, pantsColor, personality, ...)
 	-- these values are zero indexed!
 
@@ -8,18 +12,18 @@ function create(name, species, genderIndex, primaryColor, accentColor, hornColor
 	local humanoidConfig = sb.jsonMerge(root.assetJson(speciesConfig.humanoidConfig or "/humanoid.config"), speciesConfig.humanoidOverrides or {})
 
 
-	genderIndex = math.fmod(genderIndex, #speciesConfig.genders)
+	genderIndex = wrap(genderIndex, speciesConfig.genders)
 	local gender = speciesConfig.genders[genderIndex + 1]
 
-	primaryColor = math.fmod(primaryColor, #speciesConfig.bodyColor)
-	accentColor = math.fmod(accentColor, #speciesConfig.accentColor)
-	bellyColor = math.fmod(bellyColor, #speciesConfig.bellyColor)
-	hornColor = math.fmod(hornColor, #speciesConfig.hornColor)
-	fluffColor = math.fmod(fluffColor, #speciesConfig.fluffColor)
-	eyeColor = math.fmod(eyeColor, #speciesConfig.eyeColor)
-	beansColor = math.fmod(beansColor, #speciesConfig.beansColor)
+	primaryColor = wrap(primaryColor, speciesConfig.bodyColor)
+	accentColor = wrap(accentColor, speciesConfig.accentColor)
+	bellyColor = wrap(bellyColor, speciesConfig.bellyColor)
+	hornColor = wrap(hornColor, speciesConfig.hornColor)
+	fluffColor = wrap(fluffColor, speciesConfig.fluffColor)
+	eyeColor = wrap(eyeColor, speciesConfig.eyeColor)
+	beansColor = wrap(beansColor, speciesConfig.beansColor)
 
-	personality = math.fmod(personality, #humanoidConfig.personalities)
+	personality = wrap(personality, humanoidConfig.personalities)
 
 	local directives = ""
 
