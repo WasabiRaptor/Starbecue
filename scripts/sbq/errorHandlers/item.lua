ef78b0650cba8d4cb5d10bedfe89523a8395abac
WasabiRaptor <ketchupraptor@gmail.com>
2025-10-21 13:14:22 -0400

    removed items to replace using error handler

--- scripts/sbq/errorHandlers/item.lua
+++ scripts/sbq/errorHandlers/item.lua
@@ -4,11 +4,18 @@ local replaceItems = {
 	sbqPotionXeronious = {name = "sbqMysteriousPotion", parameters = {identity = {species = "sbq/Xeronious/Kaiju"}}},
 	sbqConsumableZiellekPearl = {name = "sbqMysteriousPotion", parameters = {identity = {species = "sbq/LakotaAmitola/ziellekDragon"}}},
 	sbqPotionSlime = { name = "sbqMysteriousPotion", parameters = { identity = { species = "sbq/slime" } } },
+	sbqPotionFray = { name = "sbqMysteriousPotion", parameters = { identity = { species = "sbq/IcyVixen/Fray" } } },
 
+	sbqCoreAvian = {name = "sbqMysteriousPotion", parameters = {identity = {species = "avian"}}},
 	sbqCoreVaporeon = {name = "sbqMysteriousPotion", parameters = {identity = {species = "sbq/vaporeonGiant"}}},
 	sbqCoreXeronious = {name = "sbqMysteriousPotion", parameters = {identity = {species = "sbq/Xeronious/Kaiju"}}},
+	sbqCoreZiellekDragon = {name = "sbqMysteriousPotion", parameters = {identity = {species = "sbq/LakotaAmitola/ziellekDragon"}}},
+	sbqCoreSlime = { name = "sbqMysteriousPotion", parameters = { identity = { species = "sbq/slime" } } },
+	sbqCoreFray_IcyVixen = { name = "sbqMysteriousPotion", parameters = { identity = { species = "sbq/IcyVixen/Fray" } } },
 
 	sbqCoreCharem = {name = "dirtmaterial"},
+
+	sbqMouseRoomTool = { name = "sbqNominomicon" },
 
 }
 function error(itemDescriptor)
