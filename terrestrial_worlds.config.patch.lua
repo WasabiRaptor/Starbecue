a45ea0b62b8ceac289427c8b6a41fca20caccb29
WasabiRaptor <ketchupraptor@gmail.com>
2025-10-30 10:32:58 -0400

    query and patch the dungeon count range correctly

--- terrestrial_worlds.config.patch.lua
+++ terrestrial_worlds.config.patch.lua
@@ -2,7 +2,7 @@
 function patch(config)
 	if sb.jsonQuery(config, "planetTypes.garden.layers.surface.dungeons") then
 		table.insert(config.planetTypes.garden.layers.surface.dungeons, { 1.0, "sbqGardenFireplace" })
-		if sb.jsonQuery(config, "config.planetTypes.garden.layers.surface.dungeonCountRange") then
+		if sb.jsonQuery(config, "planetTypes.garden.layers.surface.dungeonCountRange") then
 			config.planetTypes.garden.layers.surface.dungeonCountRange[1] = #config.planetTypes.garden.layers.surface.dungeons
 			config.planetTypes.garden.layers.surface.dungeonCountRange[2] = #config.planetTypes.garden.layers.surface.dungeons
 		end
