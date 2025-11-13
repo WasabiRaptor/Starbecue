b2773ea57dc8d89999fd2b4da48a8f98edf4d313
WasabiRaptor <ketchupraptor@gmail.com>
2025-11-13 07:38:58 -0500

    move a bunch of content to SBQ-LokiVulpix repo

--- player.config.patch.lua
+++ player.config.patch.lua
@@ -1,27 +1,7 @@
 function patch(config)
-    config = sb.jsonMerge(config, {
+	config = sb.jsonMerge(config, {
 		defaultCodexes = {
-			["sbq/LokiVulpix/Auri"] = { "protectorate1", "protectorate2" },
-			["sbq/LokiVulpix/Loki"] = { "protectorate1", "protectorate2" },
-			["sbq/Auroria/Auroria"] = { "protectorate1", "protectorate2" },
-			["sbq/ambi_sykhar/Ambi"] = { "protectorate1", "protectorate2" },
-			["sbq/Zygahedron/Zevi"] = { "protectorate1", "protectorate2" },
-			["sbq/Fevix/Sandy"] = { "protectorate1", "protectorate2" },
-			["sbq/vaporeonGiant"] = { "protectorate1", "protectorate2" },
-			["sbq/LokiVulpix/Socks"] = { "protectorate1", "protectorate2" },
-			["sbq/FFWizard/Helena"] = { "protectorate1", "protectorate2" },
-			["sbq/FFWizard/hellhound"] = { "protectorate1", "protectorate2" },
-			["sbq/LokiVulpix/Clover"] = { "protectorate1", "protectorate2" },
-			["sbq/Xeronious/Batty"] = { "protectorate1", "protectorate2" },
-			["sbq/Ferrilata_/Ferri"] = { "protectorate1", "protectorate2" },
-			["sbq/AkariKaen/Akari"] = { "protectorate1", "protectorate2" },
-			["sbq/slime"] = { "protectorate1", "protectorate2" },
-			["sbq/Xeronious/Kaiju"] = { "protectorate1", "protectorate2" },
-			["sbq/IcyVixen/Fray"] = { "protectorate1", "protectorate2" },
-			["sbq/LakotaAmitola/ziellekDragon"] = { "protectorate1", "protectorate2" },
-			["sbq/LakotaAmitola/Lakota"] = { "protectorate1", "protectorate2" },
-			["sbq/Bestethan/Dex"] = { "protectorate1", "protectorate2" },
-			["sbq/naga"] = { "protectorate1", "protectorate2" }
+			["sbq/Ferrilata_/Ferri"] = { "protectorate1", "protectorate2" }
 		},
 		statusControllerSettings = {
 			stats = {
@@ -54,8 +34,8 @@ function patch(config)
 				}
 			}
 		}
-    })
-    local blueprints = {
+	})
+	local blueprints = {
 		{ item= "sbqController" },
 		{ item= "sbqRequester" },
 		{ item= "sbqCandy", parameters={level=1} },
@@ -64,14 +44,14 @@ function patch(config)
 		{ item= "sbqCandy", parameters={level=4} },
 		{ item= "sbqCandy", parameters={level=5} },
 		{ item= "sbqCandy", parameters={level=6} }
-    }
-    for _, v in ipairs(blueprints) do
-        table.insert(config.defaultBlueprints.tier1, v)
-    end
-    table.insert(config.statusControllerSettings.primaryScriptSources, "/scripts/player/SBQ_player_primary.lua")
-    table.insert(config.companionsConfig.scripts, "/scripts/player/SBQ_player_companion.lua")
-    table.insert(config.deploymentConfig.scripts, "/scripts/player/SBQ_player_deployment.lua")
-    config.genericScriptContexts.starbecue = "/scripts/player/SBQ_player_generic.lua"
+	}
+	for _, v in ipairs(blueprints) do
+		table.insert(config.defaultBlueprints.tier1, v)
+	end
+	table.insert(config.statusControllerSettings.primaryScriptSources, "/scripts/player/SBQ_player_primary.lua")
+	table.insert(config.companionsConfig.scripts, "/scripts/player/SBQ_player_companion.lua")
+	table.insert(config.deploymentConfig.scripts, "/scripts/player/SBQ_player_deployment.lua")
+	config.genericScriptContexts.starbecue = "/scripts/player/SBQ_player_generic.lua"
 
-    return config
+	return config
 end
